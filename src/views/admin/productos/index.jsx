b2ca27222a5483158672852932d5cdf8a42beeb8
lulo <lopez.gallego2611@gmail.com>
2025-04-11 11:16:14 -0500

    commit

--- src/views/admin/productos/index.jsx
+++ src/views/admin/productos/index.jsx
@@ -1,5 +1,6 @@
 import plimon from "assets/img/productos/plimon.jpg";
 import ProductCard from "./components/ProductCard";
+import ModalExample from "./components/modalProducto";
 
 const Productos = () => {
   return (
@@ -7,12 +8,8 @@ const Productos = () => {
       <div className="col-span-1 h-fit w-full xl:col-span-1 2xl:col-span-2">
         <div className="mb-4 mt-5 flex flex-col justify-between px-4 md:flex-row md:items-center">
           <h4 className="ml-1 text-2xl font-bold text-navy-700 dark:text-white">
-            Nuestros productos  <button
-            href=""
-            className="ml-5 linear rounded-[20px] bg-brand-900 px-4 py-2 text-base font-medium text-white transition duration-200 hover:bg-brand-800 active:bg-brand-700 dark:bg-brand-400 dark:hover:bg-brand-300 dark:active:opacity-90"
-          >
-            Agregar Nuevo
-          </button>
+            Nuestros productos <ModalExample>  
+               </ModalExample>
           </h4>
         </div>
         <div className="grid gap-5 md:grid-cols-3">
@@ -23,7 +20,6 @@ const Productos = () => {
           />
         </div>
       </div>
-
     </div>
   );
 };
